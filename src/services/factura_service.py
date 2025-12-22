2b5381fca093831ccfb56c94303aa4b972c457d4
Mateo Pastor <matesebas1987@gmail.com>
2025-12-21 21:48:08 -0500

    Implementar calculo de subtotal e IVA

---
+++ src/services/factura_service.py
@@ -0,0 +1,17 @@
+class FacturaService:
+    def __init__(self, factura_repository):
+        self.factura_repository = factura_repository
+        self.IVA_PERCENT = 0.15
+
+    def generar_detalle_factura(self, monto_base):
+        if monto_base <= 0:
+            raise ValueError("El monto base debe ser mayor a cero")
+        
+        iva = monto_base * self.IVA_PERCENT
+        total = monto_base + iva
+
+        return {
+            "subtotal" : round(monto_base,2),
+            "I.V.A" : round(iva, 2),
+            "total" : round(total,2)
+        }
