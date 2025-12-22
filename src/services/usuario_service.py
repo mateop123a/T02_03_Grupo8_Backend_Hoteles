d89147ab7c04851bae324f9198c6b36e8f1d37e1
Mateo Pastor <matesebas1987@gmail.com>
2025-12-22 18:17:28 -0500

    Actualizacion de codigo para validacion de campos

--- src/services/usuario_service.py
+++ src/services/usuario_service.py
@@ -1,28 +1,36 @@
 import re
 
 class UsuarioService:
-    def __init__(self, repository):
-        self.repository = repository
-        self.roles = ["Administrador", "Recepcionista", "Contador"]
+    ROLES_VALIDOS = ["Administrador", "Recepcionista", "Contador"]
+    def __init__(self, usuario_repository):
+        self.usuario_repository = usuario_repository
 
-    def validacionCorreo(self, correo):
+    def _validar_correo(self, correo:str):
         pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
         if not re.match(pattern, correo):
             raise ValueError("El correo electronico no es valido")
     
-    def registrarUsuario(self, datos_usuario):
+    def _validar_campos_obligatorios(self, datos_usuario):
         campos = ["nombre", "rol", "username", "password", "correo"]
         for campo in campos:
             if not datos_usuario.get(campo):
                 raise ValueError(f"El campo {campo} es obligatorio")
+    
+    def _validar_rol(self, rol):
+        if rol not in self.ROLES_VALIDOS:
+            raise ValueError(
+                f"Rol no valido. Debe ser: {', '.join(self.ROLES_VALIDOS)}"
+            )
         
-        if datos_usuario["rol"] not in self.roles:
-            raise ValueError(f"Rol no valido. Debe ser: {', '.join(self.roles)}")
-        self.validacionCorreo(datos_usuario["correo"])
+    def registrar_usuario(self, datos_usuario:dict):
+        self._validar_campos_obligatorios(datos_usuario)
+        self._validar_rol(datos_usuario["rol"])
+        self._validar_correo(datos_usuario["correo"])
 
-        if self.repository.existeCorreo(datos_usuario["correo"]):
-            raise ValueError("Este correo ya ha sido registrado")
-
-        if self.repository.existeUsuario(datos_usuario["username"]):
+        if self.usuario_repository.existe_correo(datos_usuario["correo"]):
+            raise ValueError("El correo ya está registrado")
+        
+        if self.usuario_repository.existe_usuario(datos_usuario["username"]):
             raise ValueError("El nombre de usuario ya existe")
-        return self.repository.save(datos_usuario)
+        
+        return self.usuario_repository(datos_usuario)
