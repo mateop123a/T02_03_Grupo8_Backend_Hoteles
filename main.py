cca7a2d2221be7f6cea928c215b2f616eb13e117
Bryan270227 <118783123+Bryan270227@users.noreply.github.com>
2025-12-22 21:41:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from src.database.database import Base, engine
 from src.models import hotel, habitacion
 
+from src.controllers.health_controller import router as health_router
+from src.controllers.hotel_controller import router as hotel_router
+
 app = FastAPI(
     title="Backend Hoteles",
     description="API para gestión de hoteles",
@@ -10,6 +13,10 @@ app = FastAPI(
 
 # Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
+
+# Registrar routers
+app.include_router(health_router)
+app.include_router(hotel_router)
 
 @app.get("/")
 def root():
