68e5667604335d04165786f1bc2f7db4df40ef01
Mateo Pastor <matesebas1987@gmail.com>
2025-12-22 19:17:50 -0500

    Actualizacion total del archivo con username y correo

--- src/repositories/usuario_repository.py
+++ src/repositories/usuario_repository.py
@@ -3,10 +3,10 @@ class UsuarioRepository:
         self.usuarios = []
 
     def existeCorreo(self, correo):
-        return any(u.get("correo") == correo for u in self.usuarios)
+        return any(u["correo"] == correo for u in self.usuarios)
     
     def existeUsuario(self, username):
-        return any(u.get("username") == username for u in self.usuarios)
+        return any(u["username"] == username for u in self.usuarios)
     
     def save(self, usuario):
         usuario["usuario_id"] = len(self.usuarios) + 1
