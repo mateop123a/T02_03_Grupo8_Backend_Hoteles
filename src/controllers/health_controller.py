1e0b4e4982225b59a83cf5e8308a10d907cc3551
Bryan Barrera <76487634+Stivbryant@users.noreply.github.com>
2025-12-22 21:02:18 -0500

    Update health_controller.py

--- src/controllers/health_controller.py
+++ src/controllers/health_controller.py
@@ -1,1 +1,14 @@
 
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/health",
+    tags=["Health Check"]
+)
+
+@router.get("/")
+def health_check():
+    return {
+        "status": "OK",
+        "message": "Backend funcionando correctamente"
+    }
